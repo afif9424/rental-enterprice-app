92dbe69cf1584e200c6d02cc460ecad9e6d67e59
LostoVayne <moises.barrera.dev@gmail.com>
2025-06-17 04:30:07 -0400

    feat: add Providers component for state management and update layout to include it

---
+++ client/src/app/provider.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import StoreProvider from "@/state/redux";
+import { ReactNode } from "react";
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return <StoreProvider>{children}</StoreProvider>;
+};
+
+export default Providers;
+
+
